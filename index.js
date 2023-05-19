15b592dab098afd21f83b8a747be8e267721f424
kokilaAravindhan <125110809+kokilaAravindhan@users.noreply.github.com>
2023-05-20 02:00:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,44 @@ var h1=document.createElement("h1");
 h1 .innerHTML="Dogs Pictures";
 h1.style.textAlign="center";
 h1.style.marginTop="30px";
+h1.style.color="blue"
 
 var div=document.createElement("div");
 div.style.margin="20px";
 div.style.textAlign="center";
 
-/*var input=document.createElement("input");
-input.setAttribute("type","text");
-input.style.width="30%";
-input.id="country";
-input.setAttribute("placeholder","Enter the country name");*/
+var arr=['chow','african','airedale','akita','basenji','great dane','pug','chihuahua'];
+
+var input=document.createElement("select");
+input.setAttribute("class","dogs-selector");
+input.id="inputselect"
+for(var j=0;j<=arr.length-1;j++){
+        var option=document.createElement("option");
+        option.value=arr[j];
+        option.innerHTML=arr[j];
+        input.append(option);
+}
 
 var linebr=document.createElement("br");
+
 var button=document.createElement("button");
 button.setAttribute("type","button");
 button.classList.add("btn","btn-primary");
 button.innerHTML="Fetch";
 button.style.marginTop="10px";
+button.style.marginLeft="10px"
 button.addEventListener("click",bar);
 
-div.append(linebr,button);
+div.append(input,button);
 document.body.append(h1,div);
 
 async function bar(){
-            //void(0);
-    let res=await fetch("https://dog.ceo/api/breeds/image/random");
+    let input=document.getElementById("inputselect").value;
+    console.log(input)
+    let res=await fetch(`https://dog.ceo/api/breed/${input}/images/random`);
     let res1= await res.json();
     var img=document.createElement("img");
-    //img.style.display="none";
+   
         console.log(res1.message);
         var div1=document.createElement("div");
         div1.style.marginTop="40px";
@@ -40,7 +50,7 @@ async function bar(){
         img.setAttribute("src",res1.message);
         img.style.width="200px";
         img.style.height="200px";
-        
+         //img.style.display="none";
         
         div1.append(img);
         document.body.append(div1);
